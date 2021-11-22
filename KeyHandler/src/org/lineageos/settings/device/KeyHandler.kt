d18853b2216017bb840cf0b6302d796af63ca453
Anushek Prasal <anushekprasal@gmail.com>
2021-11-22 16:55:50 +0530

    KeyHandler: Allow to mute media volume with alert slider [2/3]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -91,7 +91,8 @@ class KeyHandler : Service() {
     }
 
     private fun handleMode(position: Int) {
-        val muteMedia = sharedPreferences.getBoolean(MUTE_MEDIA_WITH_SILENT, false)
+        val muteMedia = Settings.System.getInt(getContentResolver(),
+                Settings.System.ALERT_SLIDER_MUTE_MEDIA, 0) == 1
 
         val mode = when (position) {
             POSITION_TOP -> sharedPreferences.getString(ALERT_SLIDER_TOP_KEY, "0")!!.toInt()
